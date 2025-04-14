a33460411c4ecf601d1e74144fb25f37d13d7bba
EsleySaab <esleysantana.sa@gmail.com>
2025-04-14 20:16:24 -0300

    chore: configure Firebase and Firestore

--- app/(project)/login/page.tsx
+++ app/(project)/login/page.tsx
@@ -3,9 +3,9 @@ import { handleAuth } from "@/app/actions/handle-auth"
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Login!</h1>
+      <h1 className="text-4xl font-bold mb-4">Login!</h1>
       <form action={handleAuth}>
-        <button type="submit">Signin with Google</button>
+        <button type="submit" className="border rounded-md px-2 cursor-pointer">Signin with Google</button>
       </form>
     </div>
   )
