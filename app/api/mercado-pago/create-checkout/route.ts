a6593d70e911f3da45aa4ec9d773aee3fe3efac4
EsleySaab <esleysantana.sa@gmail.com>
2025-04-17 15:03:46 -0300

    feat: add mercado-pago api routes

---
+++ app/api/mercado-pago/create-checkout/route.ts
@@ -0,0 +1,80 @@
+import mpClient from "@/app/lib/mercado-pago"
+import { error } from "console"
+import { Preference } from "mercadopago"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST(request: NextRequest) {
+  const { testeId, userEmail } = await request.json()
+
+  try {
+    const preference = new Preference(mpClient)
+
+    const createdPreference = await preference.create({
+      body: {
+        external_reference: testeId, // Isso impacta na pontuação do Mercado Pago
+        metadata: {
+          testeId, // Essa variável é convertida para snake_case --> teste_id
+        },
+        ...(userEmail && { payer: { email: userEmail } }), // Adiciona o e-mail do usuário se fornecido | Também é importante para pontuação
+        items: [
+          {
+            id: "",
+            description: "",
+            title: "",
+            quantity: 1,
+            unit_price: 1,
+            currency_id: "BRL",
+            category_id: "services",
+          },
+        ],
+        payment_methods: {
+          installments: 12,
+          // excluded_payment_methods: [
+          //   {
+          //     id: "bolbradesco",
+          //   },
+          //   {
+          //     id: "pec",
+          //   },
+          // ],
+          // excluded_payment_types: [
+          //   {
+          //     id: "debit_card",
+          //   },
+          //   {
+          //     id: "credit_card",
+          //   },
+          // ],
+        },
+        auto_return: "approved",
+        back_urls: {
+          success: `${process.env.NEXT_PUBLIC_BASE_URL}api/mercado-pago/pending`,
+          failure: `${process.env.NEXT_PUBLIC_BASE_URL}api/mercado-pago/pending`,
+          pending: `${process.env.NEXT_PUBLIC_BASE_URL}api/mercado-pago/pending`,
+        },
+      },
+    })
+
+    if (!createdPreference.id) {
+      return NextResponse.json(
+        {
+          error: "Erro ao criar checkout com Mercado Pago",
+        },
+        { status: 500 }
+      )
+    }
+
+    return NextResponse.json({
+      preferenceId: createdPreference.id,
+      initPoint: createdPreference.init_point,
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      {
+        error: "Erro ao criar checkout com Mercado Pago",
+      },
+      { status: 500 }
+    )
+  }
+}
---
+++ app/api/mercado-pago/webhook/route.ts
@@ -0,0 +1,44 @@
+import mpClient, { validateMercadoPagoWebhook } from "@/app/lib/mercado-pago"
+import { handleMercadoPagoPayment } from "@/app/server/mercado-pago/handle-mercado-pago-payment"
+import { Payment } from "mercadopago"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function POST(request: NextRequest) {
+  try {
+    validateMercadoPagoWebhook(request)
+
+    const body = await request.json()
+
+    const { type, data } = body
+
+    // Webhook aqui
+
+    switch (type) {
+      case "payment":
+        const payment = new Payment(mpClient)
+        const paymentData = await payment.get({
+          id: data.id,
+        })
+
+        if (
+          paymentData.status === "approved" ||
+          paymentData.date_approved !== null
+        ) {
+          await handleMercadoPagoPayment(paymentData)
+        }
+        break
+      case "subscription_preapproval": // Eventos de assinatura
+        break
+      default:
+        console.log("Esse evento não é suportado")
+    }
+
+    return NextResponse.json({ received: true }, { status: 200 })
+  } catch (error) {
+    console.error("Error handling webhook:", error)
+    return NextResponse.json(
+      { error: "Webhook handler failed" },
+      { status: 500 }
+    )
+  }
+}
